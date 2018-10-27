69e10c5cf8cff77478cc10b553c678dd2938d1d3
wongco <ginson@gmail.com>
2018-10-26 21:47:05 -0700

    Complete functions and chai tests

--- iterators.js
+++ iterators.js
@@ -1,57 +1,136 @@
 // Write a function called listNames which takes in an array of songs and console.logs the name of each one.
 
+function listNames(songsArr) {
+  songsArr.forEach(function(song) {
+    console.log(song.name);
+  });
+}
+
+//listNames(songs);
+
 // Write a function called listSongDetails which takes in an array of songs and console.logs details about each one. The details should be in the following example format: "Smooth, by Santana featuring Rob Thomas (4:00)".
+
+function listSongDetails(songsArr) {
+  songsArr.forEach(function(song) {
+    console.log(`${song.name}, by ${song.artist} ${song.duration}`);
+  });
+}
+
+//listSongDetails(songs);
 
 // Write a function called summerJamCount which takes in an array of songs and returns the number of songs which hit #1 on the charts in June, July, or August.
 
+function summerJamCount(songsArr) {
+  return songsArr.filter(function(song) {
+    return song.month >= 6 && song.month <= 8;
+  }).length;
+}
+
+// summerJamCount(songs);
+
 // Write a function called getDurations which takes in an array of songs and returns an array of each song's duration.
+
+function getDurations(songsArr) {
+  return songsArr.map(function(song) {
+    return song.duration;
+  });
+}
+
+// getDurations(songs);
 
 // Write a function called getDurationInSeconds which takes in an array of songs and returns an array of each song's duration in seconds.
 
+function getDurationsInSeconds(songsArr) {
+  return songsArr.map(function(song) {
+    var durationArr = song.duration.split(':');
+    return parseInt(durationArr[0]) * 60 + parseInt(durationArr[1]);
+  });
+}
+
+//getDurationsInSeconds(songs);
+
 // Write a function called getMainArtists which takes in an array of songs and returns an array of the primary artists on the recordings. If there's only one artist, that artist should be returned; if there are featured artists, they should be ignored (so only the artist to the left of "featuring" is kept.)
+
+function getMainArtists(songsArr) {
+  return songsArr.map(function(songs) {
+    return songs.artist.split(' featuring ')[0];
+  });
+}
+
+//getMainArtists(songs);
 
 // Write a function called getBigHits which takes an array of songs and returns an array of songs which were number one for 10 or more weeks.
 
+function getBigHits(songsArr) {
+  return songsArr.filter(function(song) {
+    return song.weeksAtNumberOne >= 10;
+  });
+}
+
+//getBigHits(songs);
+
 // Write a function called getShortSongs which takes an array of songs and returns an array of songs which shorter than 3 minutes.
 
-// Write a function called getSongsByArtist which takes in an array of artists and the name of an artist and returns an array of songs by that artist.
+function getShortSongs(songsArr) {
+  return songsArr.filter(function(song) {
+    var durationArr = song.duration;
+    return parseInt(durationArr[0]) < 3;
+  });
+}
+
+//getShortSongs(songs);
+
+// Write a function called getSongsByArtist which takes in an array of songs and the name of an artist and returns an array of songs by that artist.
+
+function getSongsByArtist(songsArr, artist) {
+  return songsArr.filter(function(song) {
+    return song.artist === artist;
+  });
+}
 
 // Refactor summerJamCount to use reduce!
+function summerJamCount(songsArr) {
+  return songsArr.reduce(function(total, song) {
+    if (song.month >= 6 && song.month <= 8) {
+      total++;
+    }
+    return total;
+  }, 0);
+}
+
+//summerJamCount(songs);
 
 // Write a function called getTotalDurationInSeconds which takes in an array of songs and returns the total amount of time (in seconds) it would take to listen to all of the songs. (Hint: can you use anything you've written already to help solve this problem?)
 
+function getTotalDurationInSeconds(songs) {
+  return getDurationInSeconds(songs).reduce(function(total, songDuration) {
+    return total + songDuration;
+  });
+}
+
+getTotalDurationInSeconds(songs);
+
 // Write a function called getSongCountByArtist which takes in an array of songs and returns an object. The keys in the object should be artist names, and the values should be the number of songs by that artist in the orignal array.
+
+function getSongCountByArtist(songsArr) {
+  return songsArr.reduce(function(songCount, song) {
+    songCount[song.artist]
+      ? songCount[song.artist]++
+      : (songCount[song.artist] = 1);
+    return songCount;
+  }, {});
+}
+
+//getSongCountByArtist(songs);
 
 // Write a function called averageWeeksAtNumberOne which takes in an array of songs and returns the average number of weeks that songs on the list were #1.
 
+function averageWeeksAtNumberOne(songsArr) {
+  return (
+    songsArr.reduce(function(totalWeeks, song) {
+      return totalWeeks + song.weeksAtNumberOne;
+    }, 0) / songsArr.length
+  );
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// averageWeeksAtNumberOne(songs);
--- iteratorsSpec.js
+++ iteratorsSpec.js
@@ -1,94 +1,93 @@
-var expect = chai.expect
+var expect = chai.expect;
 
-describe("#getDurations", function(){
-  it("should return the same length of the original array", function(){
-    expect(getDurations(songs).length).to.equal(199)
+describe('#getDurations', function() {
+  it('should return the same length of the original array', function() {
+    expect(getDurations(songs).length).to.equal(199);
   });
-  it("should return the correct format for each song", function(){
-      expect(getDurations(songs)[0]).to.equal("4:00")
-      expect(getDurations(songs)[1]).to.equal("3:20")
-      expect(getDurations(songs)[2]).to.equal("4:10")
-      expect(getDurations(songs)[3]).to.equal("4:17")
-      expect(getDurations(songs)[198]).to.equal("4:55")
+  it('should return the correct format for each song', function() {
+    expect(getDurations(songs)[0]).to.equal('4:00');
+    expect(getDurations(songs)[1]).to.equal('3:20');
+    expect(getDurations(songs)[2]).to.equal('4:10');
+    expect(getDurations(songs)[3]).to.equal('4:17');
+    expect(getDurations(songs)[198]).to.equal('4:55');
   });
 });
 
-describe("#getDurationsInSeconds", function(){
-  it("should return the same length of the original array", function(){
-    expect(getDurationsInSeconds(songs).length).to.equal(199)
+describe('#getDurationsInSeconds', function() {
+  it('should return the same length of the original array', function() {
+    expect(getDurationsInSeconds(songs).length).to.equal(199);
   });
-  it("should return the correct format for each song", function(){
-      expect(getDurationsInSeconds(songs)[0]).to.equal(240)
-      expect(getDurationsInSeconds(songs)[1]).to.equal(200)
-      expect(getDurationsInSeconds(songs)[2]).to.equal(250)
-      expect(getDurationsInSeconds(songs)[3]).to.equal(257)
-      expect(getDurationsInSeconds(songs)[198]).to.equal(295)
-  });
-});
-
-describe("#getMainArtists", function(){
-  it("should return the same length of the original array", function(){
-    expect(getMainArtists(songs).length).to.equal(199)
-  });
-  it("should return the correct artist for each song", function(){
-      expect(getMainArtists(songs)[0]).to.equal("Santana")
-      expect(getMainArtists(songs)[1]).to.equal("Christina Aguilera")
-      expect(getMainArtists(songs)[2]).to.equal("Savage Garden")
-      expect(getMainArtists(songs)[3]).to.equal("Mariah Carey")
-      expect(getMainArtists(songs)[198]).to.equal("Adele")
+  it('should return the correct format for each song', function() {
+    expect(getDurationsInSeconds(songs)[0]).to.equal(240);
+    expect(getDurationsInSeconds(songs)[1]).to.equal(200);
+    expect(getDurationsInSeconds(songs)[2]).to.equal(250);
+    expect(getDurationsInSeconds(songs)[3]).to.equal(257);
+    expect(getDurationsInSeconds(songs)[198]).to.equal(295);
   });
 });
 
-describe("#getBigHits", function(){
-  it("should return all of the number one hits for more than 10 weeks", function(){
-    expect(getBigHits(songs).length).to.equal(19)
+describe('#getMainArtists', function() {
+  it('should return the same length of the original array', function() {
+    expect(getMainArtists(songs).length).to.equal(199);
   });
-  it("should return the correct big hit", function(){
-      expect(getBigHits(songs)[0].name).to.equal("Smooth")
-      expect(getBigHits(songs)[0].month).to.equal(10)
-      expect(getBigHits(songs)[0].year).to.equal(1999)
+  it('should return the correct artist for each song', function() {
+    expect(getMainArtists(songs)[0]).to.equal('Santana');
+    expect(getMainArtists(songs)[1]).to.equal('Christina Aguilera');
+    expect(getMainArtists(songs)[2]).to.equal('Savage Garden');
+    expect(getMainArtists(songs)[3]).to.equal('Mariah Carey');
+    expect(getMainArtists(songs)[198]).to.equal('Adele');
   });
 });
 
-describe("#getShortSongs", function(){
-  it("should return all of the short songs", function(){
-      expect(getShortSongs(songs).length).to.equal(1)
-    });
-    it("should return the correct short song", function(){
-        expect(getShortSongs(songs)[0].artist).to.equal("OMI")
-        expect(getShortSongs(songs)[0].month).to.equal(7)
-        expect(getShortSongs(songs)[0].year).to.equal(2015)
-    });
+describe('#getBigHits', function() {
+  it('should return all of the number one hits for more than 10 weeks', function() {
+    expect(getBigHits(songs).length).to.equal(19);
+  });
+  it('should return the correct big hit', function() {
+    expect(getBigHits(songs)[0].name).to.equal('Smooth');
+    expect(getBigHits(songs)[0].month).to.equal(10);
+    expect(getBigHits(songs)[0].year).to.equal(1999);
+  });
 });
 
-describe("#getSongsByArtist", function(){
-  it("should find an artist correctly", function(){
-      expect(getSongsByArtist(songs,'OMI').length).to.equal(1)
-      expect(getSongsByArtist(songs,'OMI')[0].name).to.equal('Cheerleader')
+describe('#getShortSongs', function() {
+  it('should return all of the short songs', function() {
+    expect(getShortSongs(songs).length).to.equal(1);
   });
-  it("should return an empty array if the artist is not found", function(){
-      expect(getSongsByArtist(songs,'WRONG').length).to.equal(0)
+  it('should return the correct short song', function() {
+    expect(getShortSongs(songs)[0].artist).to.equal('OMI');
+    expect(getShortSongs(songs)[0].month).to.equal(7);
+    expect(getShortSongs(songs)[0].year).to.equal(2015);
+  });
+});
+
+describe('#getSongsByArtist', function() {
+  it('should find an artist correctly', function() {
+    expect(getSongsByArtist(songs, 'OMI').length).to.equal(1);
+    expect(getSongsByArtist(songs, 'OMI')[0].name).to.equal('Cheerleader');
+  });
+  it('should return an empty array if the artist is not found', function() {
+    expect(getSongsByArtist(songs, 'WRONG').length).to.equal(0);
   });
 });
 
 // change xit to it so that your tests are no longer pending!
 
-describe("#summerJamCount", function(){
-  xit("needs you to write some tests!", function(){
+describe('#summerJamCount', function() {
+  it('should find the correct amount of songs', function() {
+    expect(summerJamCount(songs)).to.equal(47);
     // WRITE YOUR OWN TESTS!
   });
 });
 
-describe("#getTotalDurationInSeconds", function(){
-  xit("needs you to write some tests!", function(){
-    // WRITE YOUR OWN TESTS!
+describe('#getTotalDurationInSeconds', function() {
+  xit('should find the correct amount of seconds', function() {
+    expect(getTotalDurationInSeconds(songs)).to.equal(46236);
   });
 });
 
-describe("#getSongCountByArtist", function(){
-  xit("needs you to write some tests!", function(){
-    // WRITE YOUR OWN TESTS!
+describe('#getSongCountByArtist', function() {
+  it('should find the correct amount of songs per artist', function() {
+    expect(getSongCountByArtist(songs)['Christina Aguilera']).to.equal(2);
   });
 });
-
-
